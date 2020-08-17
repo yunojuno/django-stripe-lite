2406940abc73e710365b15158188ffc26d8840cf
Hugo Rodger-Brown <hugo@yunojuno.com>
2020-08-17 22:20:26 +0100

    Add support for Django 3.1 JSONField change

--- tests/settings.py
+++ tests/settings.py
@@ -15,16 +15,23 @@ DEBUG = True
 
 SECRET_KEY = "TOP_SECRET"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": getenv("TEST_DB_NAME", "postgres"),
-        "USER": getenv("TEST_DB_USER", "postgres"),
-        "PASSWORD": getenv("TEST_DB_PASSWORD", "postgres"),
-        "HOST": getenv("TEST_DB_HOST", "localhost"),
-        "PORT": getenv("TEST_DB_PORT", "5432"),
+try:
+    from django.db.models import JSONField  # noqa: F401
+
+    DATABASES = {
+        "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "test.db",}
     }
-}
+except ImportError:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": getenv("TEST_DB_NAME", "stripe"),
+            "USER": getenv("TEST_DB_USER", "postgres"),
+            "PASSWORD": getenv("TEST_DB_PASSWORD", "postgres"),
+            "HOST": getenv("TEST_DB_HOST", "localhost"),
+            "PORT": getenv("TEST_DB_PORT", "5432"),
+        }
+    }
 
 INSTALLED_APPS = (
     "django.contrib.admin",
