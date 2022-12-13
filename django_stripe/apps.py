be9f169c1da2edf58dcf2188755e290f046a3372
Darian Moody <darian.moody@yunojuno.com>
2022-12-13 21:05:55 +0000

    Patch 4.0.0 to fix apps.py (#6)
    Move the AppConfig to a location which will automatically be found by Django 3.2+.
    
    We promised to do this as part of the 4.0.0 release but somehow I managed to revert the change that actually did it before merge. This fixes that.
    
    Release 4.0.1

---
+++ django_stripe/apps.py
@@ -0,0 +1,24 @@
+import stripe
+from django.apps import AppConfig as DjangoAppConfig
+
+import django_stripe
+
+
+class AppConfig(DjangoAppConfig):
+
+    name = "django_stripe"
+    verbose_name = "Stripe"
+
+    def ready(self) -> None:
+        from . import settings
+
+        # Stripe library setup
+        stripe.api_key = settings.SECRET_KEY
+        stripe.api_version = settings.API_VERSION
+
+        # Identify the plugin to Stripe
+        stripe.set_app_info(
+            "django-stripe-lite",
+            version=django_stripe.__version__,
+            url="https://github.com/yunojuno/django-stripe-lite",
+        )
